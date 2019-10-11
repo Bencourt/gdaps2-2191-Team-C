866d880b6f11bd84741a606be64b0b07e503ac64
bjl4618 <bjl4618@g.rit.edu>
2019-10-11 16:24:03 -0400

    created the character class

---
+++ TeamTube/TeamTube/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamTube
+{
+    abstract class Character : iDamageable
+    {
+        int health;
+
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
+        
+    }
+}
