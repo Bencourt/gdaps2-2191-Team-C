cf8d4226e72ef06b3babcd067474971784da33ff
Nick Pels <nxp2196@rit.edu>
2019-10-11 18:05:15 -0400

    Rough outline for menu controller

---
+++ TeamTube/TeamTube/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamTube
+{
+    class MenuController
+    {
+        ///Architecture
+        ///Main menu-press space or return to play
+        ///pause menu-press space to return to gameplay and x to quit
+        ///attack menu-different possibilities
+        ///     either open up new page or bring up new object-probably object
+        ///     if object-new state, 3 different options
+        ///     fsm for scrolling
+    }
+}
