815b0d139c873a60a8499928f2532ffb5f7e7627
jsc5294 <jsc5294@g.rit.edu>
2019-10-23 18:56:59 -0400

    level editor currently ONLY LOADS from LevelExample, can create new level (of only walls). Conflict with assigning button events manually.

--- LevelEditor/LevelEditor/Form1.Designer.cs
+++ LevelEditor/LevelEditor/Form1.Designer.cs
@@ -28,120 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.filePathInput = new System.Windows.Forms.TextBox();
-            this.filePathLabel = new System.Windows.Forms.Label();
-            this.saveButton = new System.Windows.Forms.Button();
-            this.levelNameInput = new System.Windows.Forms.TextBox();
-            this.levelNameLabel = new System.Windows.Forms.Label();
-            this.wallButton = new System.Windows.Forms.Button();
-            this.floorButton = new System.Windows.Forms.Button();
-            this.entranceButton = new System.Windows.Forms.Button();
-            this.exitButton = new System.Windows.Forms.Button();
+            this.levelSaveFileDialouge = new System.Windows.Forms.SaveFileDialog();
+            this.loadLevelDialouge = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wallToolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.floorToolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.entranceToolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
-            // filePathInput
+            // levelSaveFileDialouge
             // 
-            this.filePathInput.Location = new System.Drawing.Point(918, 36);
-            this.filePathInput.Margin = new System.Windows.Forms.Padding(2);
-            this.filePathInput.Name = "filePathInput";
-            this.filePathInput.Size = new System.Drawing.Size(143, 20);
-            this.filePathInput.TabIndex = 0;
-            this.filePathInput.TextChanged += new System.EventHandler(this.filePathInput_TextChanged);
+            this.levelSaveFileDialouge.DefaultExt = "txt";
+            this.levelSaveFileDialouge.FileName = "MyLevel";
+            this.levelSaveFileDialouge.InitialDirectory = "../../../../TeamTube/TeamTube/Levels/";
+            this.levelSaveFileDialouge.Title = "save level ";
             // 
-            // filePathLabel
+            // loadLevelDialouge
             // 
-            this.filePathLabel.AutoSize = true;
-            this.filePathLabel.Location = new System.Drawing.Point(864, 36);
-            this.filePathLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.filePathLabel.Name = "filePathLabel";
-            this.filePathLabel.Size = new System.Drawing.Size(51, 13);
-            this.filePathLabel.TabIndex = 1;
-            this.filePathLabel.Text = "File Path:";
+            this.loadLevelDialouge.FileName = "MyLevel";
+            this.loadLevelDialouge.InitialDirectory = "../../../../TeamTube/TeamTube/Levels/";
+            this.loadLevelDialouge.FileOk += new System.ComponentModel.CancelEventHandler(this.LoadLevelDialouge_FileOk);
             // 
-            // saveButton
+            // menuStrip
             // 
-            this.saveButton.Location = new System.Drawing.Point(918, 62);
-            this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(143, 49);
-            this.saveButton.TabIndex = 2;
-            this.saveButton.Text = "Save to:\r\n";
-            this.saveButton.UseVisualStyleBackColor = true;
-            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.toolsToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(920, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
             // 
-            // levelNameInput
+            // fileToolStripMenuItem
             // 
-            this.levelNameInput.Location = new System.Drawing.Point(918, 11);
-            this.levelNameInput.Name = "levelNameInput";
-            this.levelNameInput.Size = new System.Drawing.Size(143, 20);
-            this.levelNameInput.TabIndex = 4;
-            this.levelNameInput.TextChanged += new System.EventHandler(this.levelNameInput_TextChanged);
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripNew,
+            this.toolStripOpen,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
             // 
-            // levelNameLabel
+            // toolStripNew
             // 
-            this.levelNameLabel.AutoSize = true;
-            this.levelNameLabel.Location = new System.Drawing.Point(848, 16);
-            this.levelNameLabel.Name = "levelNameLabel";
-            this.levelNameLabel.Size = new System.Drawing.Size(67, 13);
-            this.levelNameLabel.TabIndex = 5;
-            this.levelNameLabel.Text = "Level Name:";
+            this.toolStripNew.Name = "toolStripNew";
+            this.toolStripNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.toolStripNew.Size = new System.Drawing.Size(180, 22);
+            this.toolStripNew.Text = "New";
+            this.toolStripNew.Click += new System.EventHandler(this.CreateLevel);
             // 
-            // wallButton
+            // toolStripOpen
             // 
-            this.wallButton.Location = new System.Drawing.Point(977, 194);
-            this.wallButton.Name = "wallButton";
-            this.wallButton.Size = new System.Drawing.Size(75, 53);
-            this.wallButton.TabIndex = 0;
-            this.wallButton.Text = "Wall";
-            this.wallButton.UseVisualStyleBackColor = true;
-            this.wallButton.Click += new System.EventHandler(this.ToolButton_Click);
+            this.toolStripOpen.Name = "toolStripOpen";
+            this.toolStripOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.toolStripOpen.Size = new System.Drawing.Size(180, 22);
+            this.toolStripOpen.Text = "Open";
+            this.toolStripOpen.Click += new System.EventHandler(this.ToolStripOpen_Click);
             // 
-            // floorButton
+            // saveToolStripMenuItem
             // 
-            this.floorButton.Location = new System.Drawing.Point(977, 253);
-            this.floorButton.Name = "floorButton";
-            this.floorButton.Size = new System.Drawing.Size(75, 53);
-            this.floorButton.TabIndex = 6;
-            this.floorButton.Text = "Floor";
-            this.floorButton.UseVisualStyleBackColor = true;
-            this.floorButton.Click += new System.EventHandler(this.ToolButton_Click);
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
             // 
-            // entranceButton
+            // saveAsToolStripMenuItem
             // 
-            this.entranceButton.Location = new System.Drawing.Point(977, 312);
-            this.entranceButton.Name = "entranceButton";
-            this.entranceButton.Size = new System.Drawing.Size(75, 53);
-            this.entranceButton.TabIndex = 7;
-            this.entranceButton.Text = "Entrance";
-            this.entranceButton.UseVisualStyleBackColor = true;
-            this.entranceButton.Click += new System.EventHandler(this.ToolButton_Click);
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as...";
             // 
-            // exitButton
+            // editToolStripMenuItem
             // 
-            this.exitButton.Location = new System.Drawing.Point(977, 371);
-            this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(75, 53);
-            this.exitButton.TabIndex = 8;
-            this.exitButton.Text = "Exit";
-            this.exitButton.UseVisualStyleBackColor = true;
-            this.exitButton.Click += new System.EventHandler(this.ToolButton_Click);
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wallToolToolStripMenuItem,
+            this.floorToolToolStripMenuItem,
+            this.entranceToolToolStripMenuItem,
+            this.exitToolToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // wallToolToolStripMenuItem
+            // 
+            this.wallToolToolStripMenuItem.Name = "wallToolToolStripMenuItem";
+            this.wallToolToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.wallToolToolStripMenuItem.Text = "Wall Tool";
+            // 
+            // floorToolToolStripMenuItem
+            // 
+            this.floorToolToolStripMenuItem.Name = "floorToolToolStripMenuItem";
+            this.floorToolToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.floorToolToolStripMenuItem.Text = "Floor Tool";
+            // 
+            // entranceToolToolStripMenuItem
+            // 
+            this.entranceToolToolStripMenuItem.Name = "entranceToolToolStripMenuItem";
+            this.entranceToolToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.entranceToolToolStripMenuItem.Text = "Entrance Tool";
+            // 
+            // exitToolToolStripMenuItem
+            // 
+            this.exitToolToolStripMenuItem.Name = "exitToolToolStripMenuItem";
+            this.exitToolToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.exitToolToolStripMenuItem.Text = "Exit Tool";
             // 
             // levelEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1073, 707);
-            this.Controls.Add(this.exitButton);
-            this.Controls.Add(this.entranceButton);
-            this.Controls.Add(this.floorButton);
-            this.Controls.Add(this.wallButton);
-            this.Controls.Add(this.levelNameLabel);
-            this.Controls.Add(this.levelNameInput);
-            this.Controls.Add(this.saveButton);
-            this.Controls.Add(this.filePathLabel);
-            this.Controls.Add(this.filePathInput);
+            this.ClientSize = new System.Drawing.Size(920, 846);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "levelEditor";
             this.Text = "Level Editor";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,15 +170,20 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox filePathInput;
-        private System.Windows.Forms.Label filePathLabel;
-        private System.Windows.Forms.Button saveButton;
-        private System.Windows.Forms.TextBox levelNameInput;
-        private System.Windows.Forms.Label levelNameLabel;
-        private System.Windows.Forms.Button wallButton;
-        private System.Windows.Forms.Button floorButton;
-        private System.Windows.Forms.Button entranceButton;
-        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.SaveFileDialog levelSaveFileDialouge;
+        private System.Windows.Forms.OpenFileDialog loadLevelDialouge;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripNew;
+        private System.Windows.Forms.ToolStripMenuItem toolStripOpen;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wallToolToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem floorToolToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem entranceToolToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolToolStripMenuItem;
     }
 }
 
--- LevelEditor/LevelEditor/Form1.cs
+++ LevelEditor/LevelEditor/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace LevelEditor
 {
@@ -28,47 +29,76 @@ namespace LevelEditor
 
     public partial class levelEditor : Form
     {
+        //IO stuff
+        StreamReader reader;
+        StreamWriter writer;
+
         //level name
         string levelName;
         //level path
         string levelPath;
+        //String to hold level data
+        string levelString;
         //toolstate
         ToolState toolState;
         //tilestate
         public TileState[,] tileState;
-
+        //array of bottons
         Button[,] tileButtons;
-
-        //instantiate buttons
-        public void InstantiateButtons(object sender, EventArgs e)
-        {
-            //make tile buttons into a 26 by 26 array of buttons
-            tileButtons = new Button[26, 26];
-
-            //for each of them
-            for( int y = 0; y < 26; y++)
-            {
-                for(int x = 0; x < 26; x++)
-                {
-                    //set the text to w
-                    tileButtons[x, y].Text = "w";
-                    //set location
-                    tileButtons[x, y].Location = new Point((30 * x) + 20, (30 * y) + 20);
-                    //set size
-                    tileButtons[x, y].Size = new Size(25, 25);
-                    //set the click event to tile button click
-                    //tileButtons[x, y].Click += tileButton_Click(tileButtons[x, y]);
-                }
-            }
-        } 
 
         public levelEditor()
         {
             InitializeComponent();
             levelName = "MyLevel";
-            levelPath = "";
+            levelPath = "../../../../TeamTube/TeamTube/Levels";
+            //set toolstate to wall initially
+            toolState = ToolState.Wall;
+            tileState = new TileState[26, 26];
         }
 
+        //instantiate buttons
+        public void CreateLevel(object sender, EventArgs e)
+        {
+            //make tile buttons into a 26 by 26 array of buttons
+            tileButtons = new System.Windows.Forms.Button[26, 26];
+            //set levelname to MyLevel
+            levelName = "MyLevel";
+            //default path is ../../../../TeamTube/TeamTube/Levels/
+            levelPath = "../../../../TeamTube/TeamTube/Levels/";
+            //for each of them
+            for ( int y = 0; y < 26; y++)
+            {
+                for(int x = 0; x < 26; x++)
+                {
+                    //add to the list of drawables
+                    tileButtons[x, y] = new System.Windows.Forms.Button();
+                    //add to form1
+                    this.Controls.Add(tileButtons[x, y]);
+                    //set size
+                    tileButtons[x, y].Size = new Size(25, 25);
+                    //set location
+                    tileButtons[x, y].Location = new Point((30 * x) + 120, (30 * y) + 50);
+                    
+                    //set tileState to wall
+                    tileState[x, y] = TileState.Wall;
+                    //back color to black
+                    tileButtons[x, y].BackColor = Color.Black;
+                    //set the text to w
+                    tileButtons[x, y].Text = "w";
+                   
+                    //set the click event to tile button click
+                    //tileButtons[x, y].Click += tileButton_Click(tileButtons[x, y]);
+
+                    tileButtons[x, y].CreateGraphics();
+                }
+            }
+        } 
+
+       
+        
+
+        // OLD CODE for the old setup
+        /*
         private void levelNameInput_TextChanged(object sender, EventArgs e)
         {
             //change the level name
@@ -107,23 +137,94 @@ namespace LevelEditor
             //save the level based on the name, path and level layout
         }
 
-        private void tileButton_Click(Button sender)
+        */
+
+        private void tileButton_Click(int x, int y)
         {
-            //change this sender's text based on what the toolstate is
+            //change this sender's text and tile state based on what the toolstate is
             switch (toolState)
             {
                 case ToolState.Wall:
-                    sender.Text = "w";
+                    tileButtons[x, y].Text = "w";
+                    tileButtons[x, y].BackColor = Color.Black;
+                    tileState[x, y] = TileState.Wall;
                     break;
                 case ToolState.Floor:
-                    sender.Text = "f";
+                    tileButtons[x, y].Text = "f";
+                    tileButtons[x, y].BackColor = Color.White;
+                    tileState[x, y] = TileState.Floor;
                     break;
                 case ToolState.Entrance:
-                    sender.Text = "e";
+                    tileButtons[x, y].Text = "e";
+                    tileButtons[x, y].BackColor = Color.Blue;
+                    tileState[x, y] = TileState.Entrance;
                     break;
                 case ToolState.Exit:
-                    sender.Text = "x";
+                    tileButtons[x, y].Text = "x";
+                    tileButtons[x, y].BackColor = Color.Green;
+                    tileState[x, y] = TileState.Exit;
                     break;
+            }
+        }
+
+        private void ToolStripOpen_Click(object sender, EventArgs e)
+        {
+            //need to load from a file
+            //create level
+            CreateLevel(sender, e);
+
+            loadLevelDialouge.ShowDialog();
+        }
+
+        private void LoadLevelDialouge_FileOk(object sender, CancelEventArgs e)
+        {
+
+            //the file is good, read it and save it in the level array
+
+            
+            //reader is set to selected file
+            reader = new StreamReader("../../../../TeamTube/TeamTube/Levels/LevelExample.txt");
+            //go character by character and set the tile to the read character
+            for(int y = 0; y < 26; y++)
+            {
+                for(int x = 0; x< 26; x++)
+                {
+                    //tile will be an ascii value for the char in the file
+                    int tile = reader.Read();
+                    switch (tile - 48)
+                    {
+                        //the ascii value for zero is 48 and the single digit numbers continue: 49, 50, etc. 
+                        //we just need to offset tile by 48
+                        case 0:
+                            //wall is 0
+                            tileButtons[x, y].Text = "w";
+                            tileButtons[x, y].BackColor = Color.Black;
+                            tileState[x, y] = TileState.Wall;
+                            break;
+                        case 1:
+                            //floor is 1
+                            tileButtons[x, y].Text = "f";
+                            tileButtons[x, y].BackColor = Color.White;
+                            tileState[x, y] = TileState.Floor;
+                            break;
+                        case 2:
+                            //entrance is 2
+                            tileButtons[x, y].Text = "e";
+                            tileButtons[x, y].BackColor = Color.Blue;
+                            tileState[x, y] = TileState.Entrance;
+                            break;
+                        case 3:
+                            //exit is 3
+                            tileButtons[x, y].Text = "x";
+                            tileButtons[x, y].BackColor = Color.Green;
+                            tileState[x, y] = TileState.Exit;
+                            break;
+                        default:
+                            tileButtons[x, y].Text = "w";
+                            tileState[x, y] = TileState.Wall;
+                            break;
+                    }
+                }
             }
         }
     }
